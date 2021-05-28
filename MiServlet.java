00a1f8ce175ec96a1f12dc834fb6359a8556fcaa
GabrielPacco <83151945+GabrielPacco@users.noreply.github.com>
2021-05-27 22:08:15 -0500

    Update MiServlet.java

--- MiServlet.java
+++ MiServlet.java
@@ -1,8 +1,7 @@
 package pe.unsa.epcc;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,10 +9,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 /**
  * Servlet implementation class MiServlet
  */
-@WebServlet("/MiServlet")
+@WebServlet("/helloServlet")
 public class MiServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -30,31 +30,31 @@ public class MiServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-        String yourName = request.getParameter("yourName");
-        
-        List<Person> persons = new ArrayList<>();   
-        Person person = new Person();
-        person.setName(yourName);
-        person.setAge("18");
-        
-        persons.add(person);
-        
-        person = new Person();
-        person.setName("Juancito");
-        person.setAge("20");
-        
-        persons.add(person);
-        
-        request.setAttribute("person", person);
-        request.setAttribute("persons", persons);
-        
-        RequestDispatcher dispatcher = 
-        		request.getRequestDispatcher("home.jsp");
+		
+		String message = request.getParameter("message");
+		String numero = request.getParameter("numero");
+		String sistema = request.getHeader("USER-AGENT");
+		
+		datos bienvenida = new datos();
+	    Date fecha = new Date();
+	    
+	    bienvenida.setMessage(message);
+		bienvenida.setNumero(numero);
+	    
+		request.setAttribute("Pag", bienvenida);
+		request.setAttribute("fecha", fecha);
+		request.setAttribute("sistema", sistema);
+		
+
+		
+		RequestDispatcher dispatcher =
+				request.getRequestDispatcher("home.jsp");
 				dispatcher.forward(request, response);
 				
-				
+		/*PrintWriter writer = response.getWriter();
+		writer.println("<h1>Hello " + yourName + "</h1>");
+		writer.close();*/
 	}
 
 	/**
