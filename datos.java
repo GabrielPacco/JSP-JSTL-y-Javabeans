bfbdb973a5f2b0a1d76fc3493b1dcdd6c6c41b35
GabrielPacco <83151945+GabrielPacco@users.noreply.github.com>
2021-05-27 22:09:23 -0500

    Update and rename Person.java to datos.java

---
+++ datos.java
@@ -0,0 +1,18 @@
+package pe.unsa.epcc;
+
+public class datos{
+	private String message;
+	private String numero;
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+}
